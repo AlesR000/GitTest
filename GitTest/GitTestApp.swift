8dbd8456e9a49fb5c2c7fe2fafeec118374813a9
Alessandro Rippa <Alessandrorippa00@outlook.it>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Alessandro Rippa on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
